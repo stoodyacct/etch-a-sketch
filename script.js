8b635962540da123baef4511c5a97e51c6809107
Stanley Yoo <roflcopter15951234678@gmail.com>
2020-08-03 06:51:42 -0400

    Changed layout

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 const container = document.querySelector('#outerDiv');
+const colours = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 createGrids = size => {
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
             let div = document.createElement('div');
             div.className = 'innerDiv';
-            div.addEventListener('mouseover', () => div.style.backgroundColor = '#3bb0e6');
+            div.style.opacity = '0.5';
             container.appendChild(div);
         }
     }
+    changeColours('rainbow');
 }
 
 clearGrid = () => {
     let innerDiv = document.querySelectorAll('.innerDiv');
-    innerDiv.forEach(div => div.style.backgroundColor = 'rgba(255, 255, 255, 0.952)');
+    innerDiv.forEach(div => div.style.background = 'rgba(255, 255, 255, 0.952)');
 }
 
-createGrids(16);
+changeColours = colour => {
+    clearGrid();
+    let innerDiv = document.querySelectorAll('.innerDiv');
+    if (colour != 'rainbow') {
+        innerDiv.forEach(div => div.addEventListener('mouseover', () => div.style.background = colour));
+    } else {
+        for (let i = 0; i < innerDiv.length; i++) {
+            innerDiv[i].addEventListener('mouseover', () => innerDiv[i].style.background = colours[Math.floor(Math.random() * colours.length)]);
+        }
+    }
+}
 
-console.log(document.querySelectorAll('.innerDiv'));
-
+createGrids(30);
 const form = document.querySelector('#form');
 form.onsubmit = (e) => {
     e.preventDefault();
@@ -28,7 +39,7 @@ form.onsubmit = (e) => {
         alert("You must enter a number greater than 1")
         clearGrid();
     } else {
-        let innerDiv = document.querySelectorAll('.innerDiv');
+        innerDiv = document.querySelectorAll('.innerDiv');
         innerDiv.forEach(child => child.remove());
         document.querySelector('#outerDiv').style.gridTemplateColumns = 'repeat(' + size + ', auto)';
         createGrids(size);
